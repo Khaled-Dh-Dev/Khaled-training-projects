718250c41360927c1740eb459aa04359d6802b2c
Khaled Daher <Khaled.dh-dev@outlook.com>
2026-05-15 19:44:06 +0300

    update: add year validation and recursive input handiling

--- Age-calculator/App.js
+++ Age-calculator/App.js
@@ -1,11 +1,25 @@
 const readline = require('readline')
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+const CurrentYear = new Date().getFullYear()
+const minYearAllowed = 1900
+//const year = parseInt(CurrentYear)
 rl.question('what is your name ? ', (name) => {
-    rl.question('what is your birth year? ', (year) => {
-        const age = 2026 - year
-        console.log(`hello ${name} , your age is excatly : ${age} , you are in the ${Math.floor(age / 10) * 10}s`)
-        if (age >= 18) { console.log('you are adult') }
-        else { console.log('you are young') }
-        rl.close()
-    })
+    const askYear = () => {
+        rl.question('what is your birth year? ', (yearBirth) => {
+            const yearOfBirth = parseInt(yearBirth)
+            if (isNaN(yearOfBirth) || yearOfBirth > CurrentYear || yearOfBirth < minYearAllowed) {
+                console.log(`
+                hello ${name}
+                [!] error: the !${yearBirth}! entered is invalid year of birth 
+                 please make sure  between ${minYearAllowed} and ${CurrentYear} `)
+                return askYear()
+            }
+            const age = CurrentYear - yearOfBirth
+            console.log(`hello ${name} , your age is excatly : ${age} , you are in the ${Math.floor(age / 10) * 10}s`)
+            if (age >= 18) { console.log('you are adult') }
+            else { console.log('you are young') }
+            rl.close()
+        })
+    }
+    askYear()
 })
